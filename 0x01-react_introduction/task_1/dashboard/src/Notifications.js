b0aae2c61e48e2a68627ba2d9efc220bf11036ac
TayArmy <111232494+TayArmy@users.noreply.github.com>
2023-07-28 03:26:04 +0100

    Update Notifications.js

--- 0x01-react_introduction/task_1/dashboard/src/Notifications.js
+++ 0x01-react_introduction/task_1/dashboard/src/Notifications.js
@@ -1,14 +1,1 @@
-import React from 'react';
-import './Notifications.css';
 
-function Notifications() {
-  return (
-    <>
-      <div className="Notifications">
-        <p>Here is the list of notifications</p>
-      </div>
-    </>
-  );
-};
-
-export default Notifications;
