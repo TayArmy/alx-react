9ca881a14fde8fdb317d808168f01c00e7d3c380
alabeni temitayo <alabenitemitayo@gmail.com>
2023-08-03 00:14:12 +0100

    inline

---
+++ 0x01-react_introduction/task_1/dashboard/src/utils.js
@@ -0,0 +1,8 @@
+export const getFullYear = () => {
+    // returns the current year
+    return new Date().getFullYear()
+}
+
+export const getFooterCopy = (isIndex=false) => {
+    return isIndex ? "Holberton School" : "Holberton School main dashboard";
+}
---
+++ 0x01-react_introduction/task_2/dashboard/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.jpg';
+import './App.css';
+import { getFooterCopy, getFullYear } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} alt='logo' height={200}></img>
+        <h1>School dashboard</h1>
+      </div>
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+        <form>
+          <label htmlFor="email">email:</label>
+          <input id="email" type="text"/>
+          <label htmlFor="password">password:</label>
+          <input id="password" type="password"/>
+          <button>OK</button>
+        </form>
+      </div>
+      <div className='App-footer'>
+        <p><em>Copyright { getFullYear() } - { getFooterCopy(true) } </em></p>
+        {/* <p>Copyright { getFullYear() } - { getFooterCopy(true) }</p> */}
+      </div>  
+    </div>
+  );
+}
+
+export default App;
