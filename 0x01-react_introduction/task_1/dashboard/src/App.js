dc70116c944e2dc877cb4b2c6778bc6e3aff53d3
TayArmy <111232494+TayArmy@users.noreply.github.com>
2023-08-07 11:01:08 +0100

    react

--- 0x01-react_introduction/task_1/dashboard/src/App.js
+++ 0x01-react_introduction/task_1/dashboard/src/App.js
@@ -1,1 +1,23 @@
+import logo from './logo.jpg';
+import './App.css';
+import { getFooterCopy, getFullYear } from './utils';
 
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} alt='logo' height={200}></img>
+        <h1>School dashboard</h1>
+      </div>
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className='App-footer'>
+        <p><em>Copyright { getFullYear() } - { getFooterCopy(true) } </em></p>
+        {/* <p>Copyright { getFullYear() } - { getFooterCopy(true) }</p> */}
+      </div>  
+    </div>
+  );
+}
+
+export default App;
