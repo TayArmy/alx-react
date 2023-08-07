6daad23eb69797573ef1947e5dc11beeb6ca03c3
TayArmy <111232494+TayArmy@users.noreply.github.com>
2023-08-07 11:07:09 +0100

    react

--- 0x01-react_introduction/task_2/dashboard/src/Notifications.js
+++ 0x01-react_introduction/task_2/dashboard/src/Notifications.js
@@ -1,1 +1,27 @@
+import React from 'react'
+import './Notifications.css'
+import { getLatestNotification } from './utils'
 
+const Notifications = () => {
+  const log = () => console.log('Close button has been clicked')
+  const style = {
+    position: "absolute",
+    right: ".25rem",
+    top: ".25rem",
+    background: "transparent",
+    border: "none",
+  }
+  return (
+    <div className='Notifications' style={{position: "relative"}}>
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data="default">New course available</li>
+        <li data="urgent">New resume available</li>
+        <li data="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification()}}></li>
+      </ul>
+      <button aria-label='Close' onClick={log} style={style}>x</button>
+    </div>
+  )
+}
+
+export default Notifications
