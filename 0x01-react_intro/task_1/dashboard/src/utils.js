fceaa333e573f1c884afdab696eca5d8c85c50c1
alabeni temitayo <alabenitemitayo@gmail.com>
2023-08-26 16:18:35 +0100

    react

--- 0x01-react_intro/task_1/dashboard/src/utils.js
+++
@@ -1,8 +0,0 @@
-export const getFullYear = () => {
-  // returns the current year
-  return new Date().getFullYear()
-}
-
-export const getFooterCopy = (isIndex=false) => {
-  return isIndex ? "Holberton School" : "Holberton School main dashboard";
-}
