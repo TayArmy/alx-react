f40bd9985a6cbae25b5f9a9e82d2a0896a54319a
alabeni temitayo <alabenitemitayo@gmail.com>
2023-09-03 01:22:48 +0100

    inline

--- 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,24 +1,27 @@
-import React from 'react'
-import BodySection from './BodySection'
-// import './BodySectionWithMarginBottom.css'
+import React, { Component } from "react";
+import BodySection from "./BodySection";
+import PropTypes from "prop-types";
+import { StyleSheet, css } from "aphrodite";
 
-import { StyleSheet, css } from 'aphrodite'
-
-const BodySectionWithMarginBottom = (props) => {
-  return (
-    <div className={css(styles.bodySectionWithMargin)}>
-        {/* BodySection takes the same props passed to `this` component */}
-      <BodySection { ...props } />
-    </div>
-  )
+class BodySectionWithMarginBottom extends Component {
+  render() {
+    return (
+      <div className={css(styles.bodySectionWithMargin)}>
+        <BodySection {...this.props} />
+      </div>
+    );
+  }
 }
 
-export default BodySectionWithMarginBottom
-
-
-/* aphrodite styles */
 const styles = StyleSheet.create({
   bodySectionWithMargin: {
-    marginBottom: '40px'
-}
-})
+    marginBottom: "40px",
+  },
+});
+
+BodySectionWithMarginBottom.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+};
+
+export default BodySectionWithMarginBottom;
